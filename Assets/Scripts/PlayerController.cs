3878ebf113f548d36168d136686d3e622dbb65ba
Rian Lima <rlimamendes@proton.me>
2026-05-17 16:23:32 -0300

    feat: adiciona movimentação para o player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D rigidBody;
+    public float moveSpeed;
+    public InputActionReference moveInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.linearVelocity = moveInput.action.ReadValue<Vector2>().normalized * moveSpeed;
+    }
+}
