7875282965423df67da2c68e9e6b2fae9607ac6b
Rian Lima <rlimamendes@proton.me>
2026-05-17 17:21:00 -0300

    feat: limita a movimentação da camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,30 @@ public class CameraController : MonoBehaviour
 {
 
     private Transform target;
+    public Transform clampMin;
+    public Transform clampMax;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         target = FindAnyObjectByType<PlayerController>().transform;
+
+        // remove da herança da camera para não acompanhar seu movimento
+        clampMin.SetParent(null);
+        clampMax.SetParent(null);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        Vector3 clampedPosition = transform.position;
+
+        // limita o movimento da câmera
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, clampMin.position.x, clampMax.position.x);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, clampMin.position.y, clampMax.position.y);
+
+        transform.position = clampedPosition;
     }
 }
