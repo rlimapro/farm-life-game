a508fae4d81f0589ea882f70166f3fb7b8a1ab39
Rian Lima <rlimamendes@proton.me>
2026-05-27 23:50:42 -0300

    feat: adiciona toolbar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,16 @@ public class PlayerController : MonoBehaviour
     public InputActionReference moveInput, actionInput;
     public Animator anim;
 
+    public enum ToolType
+    {
+        plough,
+        wateringCan,
+        seeds,
+        basket
+    }
+
+    public ToolType currentTool;
+
     private void OnEnable()
     {
         moveInput.action.Enable();
@@ -42,6 +52,33 @@ public class PlayerController : MonoBehaviour
             transform.localScale = Vector3.one;
         }
 
+        if (Keyboard.current.tabKey.wasPressedThisFrame)
+        {
+            currentTool++;
+
+            if((int) currentTool >= 4)
+            {
+                currentTool = ToolType.plough;
+            }
+        }
+
+        if (Keyboard.current.digit1Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.plough;
+        }
+        if (Keyboard.current.digit2Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.wateringCan;
+        }
+        if (Keyboard.current.digit3Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.seeds;
+        }
+        if (Keyboard.current.digit4Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.basket;
+        }
+
         if (actionInput.action.WasPressedThisFrame())
         {
             UseTool();
@@ -56,6 +93,28 @@ public class PlayerController : MonoBehaviour
 
         block = FindAnyObjectByType<GrowBlock>();
 
-        block.PloughSoil();
+        // block.PloughSoil();
+
+        if(block != null)
+        {
+            switch (currentTool)
+            {
+                case ToolType.plough:
+                    block.PloughSoil();
+                    break;
+
+                case ToolType.wateringCan:
+
+                break;
+
+                case ToolType.seeds:
+
+                break;
+
+                case ToolType.basket:
+
+                break;
+            }
+        }
     }
 }
