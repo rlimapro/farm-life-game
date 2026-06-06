a66b1db4e69dd3dfdc53568d1d82f774b68f70ef
Rian Lima <rlimamendes@proton.me>
2026-06-06 02:00:51 -0300

    feat: adiciona geração de grid de seleção

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+
+    public Transform minPoint, maxPoint;
+    public GrowBlock baseGridBlock;
+    private Vector2Int gridSize;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateGrid()
+    {
+        minPoint.position = new Vector3(
+            Mathf.Round(minPoint.position.x), 
+            Mathf.Round(minPoint.position.y), 
+            0f
+        );
+
+        maxPoint.position = new Vector3(
+            Mathf.Round(maxPoint.position.x), 
+            Mathf.Round(maxPoint.position.y), 
+            0f
+        );
+
+        Vector3 startpoint = minPoint.position + new Vector3(.5f, .5f, 0f);
+
+        gridSize = new Vector2Int(
+            Mathf.RoundToInt(maxPoint.position.x - minPoint.position.x), 
+            Mathf.RoundToInt(maxPoint.position.y - minPoint.position.y)    
+        );
+
+        for(int y = 0; y < gridSize.y; y++)
+        {
+            for(int x = 0; x < gridSize.x; x++)
+            {
+                GrowBlock newBlock = Instantiate(baseGridBlock, startpoint + new Vector3(x, y, 0f), Quaternion.identity);
+
+                newBlock.transform.SetParent(transform);
+            }
+        }
+
+        baseGridBlock.gameObject.SetActive(false);
+
+    }
+}
