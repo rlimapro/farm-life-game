dc10d577ad72dfc4c5faf3e1a66d57df638a4286
Rian Lima <rlimamendes@proton.me>
2026-06-11 01:20:35 -0300

    feat: adiciona lógica de dormir para avançar crescimento da plantação

--- Assets/Scripts/GridInfo.cs
+++ Assets/Scripts/GridInfo.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using System.Collections.Generic;
 
 public class GridInfo : MonoBehaviour
@@ -39,6 +40,38 @@ public class GridInfo : MonoBehaviour
     {
         theGrid[yPos].blocks[xPos].currentStage = theBlock.currentStage;
         theGrid[yPos].blocks[xPos].isWatered = theBlock.isWatered;
+    }
+
+    public void GrowCrop()
+    {
+        for(int y = 0; y < theGrid.Count; y++)
+        {
+            for(int x = 0; x < theGrid[y].blocks.Count; x++)
+            {
+                if(theGrid[y].blocks[x].isWatered == true)
+                {
+                    switch (theGrid[y].blocks[x].currentStage)
+                    {
+                        case GrowBlock.GrowthStage.planted:
+
+                            theGrid[y].blocks[x].currentStage = GrowBlock.GrowthStage.growing1;
+                            break;
+
+                        case GrowBlock.GrowthStage.growing1:
+
+                            theGrid[y].blocks[x].currentStage = GrowBlock.GrowthStage.growing2;
+                            break;
+
+                        case GrowBlock.GrowthStage.growing2:
+
+                            theGrid[y].blocks[x].currentStage = GrowBlock.GrowthStage.ripe;
+                            break;
+                    }
+
+                    theGrid[y].blocks[x].isWatered = false;
+                }
+            }
+        }
     }
 }
 
